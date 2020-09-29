27e4351a13d3e3786e89840278ded1d179a7b415
JGrenet <jeremy.grenet@epitech.eu>
2020-09-29 23:12:11 +0200

    feat(shell): home component

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,12 +1,15 @@
 import { observer } from "mobx-react-lite";
 import React from "react";
 import useStores from "../../stores";
+import Button from "../button/Button";
 
 const Home = observer(() => {
-    const { responsiveStore } = useStores();
+    const { localeStore, responsiveStore } = useStores();
+    const appKeys = localeStore.keys;
 
     return (
         <div className="home tab-content">
+            
             <div className="home_content">
                 <div className="home_content__main-content">
                     <h1 className="bold">Bonjour!</h1>
@@ -20,7 +23,11 @@ const Home = observer(() => {
             <div className="home_image">
                 <img src="./img/photo_pro.jpg" alt="photo_pro"/>
             </div>
-            {responsiveStore.isMobile && <div className="home_contact"></div>}
+            {responsiveStore.isMobile && (
+                <div className="home_contact">
+                    <Button label={appKeys["ACTION_CONTACT_ME"]} />
+                </div>
+            )}
         </div>       
     )
 });
