6826379f7f90f232d1809f9234ace3db30df9231
JGrenet <jeremy.grenet@epitech.eu>
2020-10-01 23:28:37 +0200

    feat(service): addind style for services items

---
+++ src/components/services/ServiceItem.tsx
@@ -0,0 +1,51 @@
+import { observer } from "mobx-react-lite";
+import React, { useCallback, useMemo } from "react";
+import clsx from "clsx";
+
+interface ServiceItemProps {
+    index: number;
+    img: string;
+    content: string;
+    selected: boolean;
+    selectItem: (index: number) => void;
+    title: string;
+}
+
+const ServiceItem = observer(({
+    index,
+    img,
+    title,
+    content,
+    selected,
+    selectItem
+}: ServiceItemProps) => {
+    
+    const handleHover = useCallback(() => {
+        selectItem(index);
+    }, [selectItem, index]);
+
+    const imgUrl = useMemo(() => selected ? img + "_selected.png" : img + ".png",
+        [selected, img]
+    );
+
+    return (
+        <div
+            className={clsx(
+                "services-list_item",
+                {["services-list_item--selected"]: selected}
+            )}
+            onMouseOver={handleHover}
+        >
+            <div className="services-list_item__icon">
+                <img src={imgUrl} alt="service_icon" />
+                <span>{title}</span>
+            </div>
+            <div className="services-list_item__separator" />
+            <div className="services-list_item__content">
+                {content}
+            </div>
+        </div>
+    )
+});
+
+export default ServiceItem;
--- src/components/services/Services.tsx
+++ src/components/services/Services.tsx
@@ -1,8 +1,9 @@
 import { observer } from "mobx-react-lite";
-import React, { CSSProperties, useCallback } from "react";
+import React, { CSSProperties, useCallback, useState } from "react";
 import { CSSTransition } from "react-transition-group";
 import { SHELL_PADDING } from "../../utils/globals";
 import useStores from "../../stores";
+import ServiceItem from "./ServiceItem";
 
 type ServicesType = {
     [key: string]: CSSProperties
@@ -13,6 +14,7 @@ const Services = observer(() => {
     const appKeys = localeStore.keys;
     const shellWidth = document.documentElement.clientWidth - (SHELL_PADDING * 2);
     const shellHeight = document.documentElement.clientHeight - (SHELL_PADDING * 2);
+    const [selectedItem, setSelectedItem] = useState<number>(0);
 
     const handleAddEnd = useCallback((node, done) => {
         node.addEventListener("transitionend", done, false);
@@ -34,39 +36,30 @@ const Services = observer(() => {
             <div className="services_content" style={{color: "white"}}>
                 <h2>My services</h2>
                 <div className="services_content__services-list services-list">
-                    <div className="services-list_item">
-                        <div className="services-list_item__icon">
-
-                        </div>
-                        <div className="services-list_item__separator">
-
-                        </div>
-                        <div className="services-list_item__content">
-
-                        </div>
-                    </div>
-                    <div className="services-list_item">
-                        <div className="services-list_item__icon">
-
-                        </div>
-                        <div className="services-list_item__separator">
-
-                        </div>
-                        <div className="services-list_item__content">
-                            
-                        </div>
-                    </div>
-                    <div className="services-list_item">
-                        <div className="services-list_item__icon">
-
-                        </div>
-                        <div className="services-list_item__separator">
-
-                        </div>
-                        <div className="services-list_item__content">
-                            
-                        </div>
-                    </div>
+                    <ServiceItem
+                        index={0}
+                        img={"/icon/web_development"}
+                        title={appKeys["SERVICES_ITEM_WEBDEV_TITLE"]}
+                        content={appKeys["SERVICES_ITEM_WEBDEV_CONTENT"]}
+                        selected={selectedItem === 0}
+                        selectItem={setSelectedItem}
+                    />
+                    <ServiceItem
+                        index={1}
+                        img={"/icon/responsive"}
+                        title={appKeys["SERVICES_ITEM_RESPONSIVE_TITLE"]}
+                        content={appKeys["SERVICES_ITEM_RESPONSIVE_CONTENT"]}
+                        selected={selectedItem === 0}
+                        selectItem={setSelectedItem}
+                    />
+                    <ServiceItem
+                        index={2}
+                        img={"/icon/back_office"}
+                        title={appKeys["SERVICES_ITEM_BACKOFFICE_TITLE"]}
+                        content={appKeys["SERVICES_ITEM_BACKOFFICE_CONTENT"]}
+                        selected={selectedItem === 0}
+                        selectItem={setSelectedItem}
+                    />
                 </div>
             </div>
             {!responsiveStore.isMobile && (
