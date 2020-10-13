3cca42b34c4ad2464515768446c6abcce1c257fd
JGrenet <jeremy.grenet@epitech.eu>
2020-10-13 10:32:47 +0200

    feat(productions): quick style poc of the tab

--- src/components/productions/Productions.tsx
+++ src/components/productions/Productions.tsx
@@ -19,6 +19,7 @@ const Productions = (): JSX.Element => {
 
     const getRecoverStyle = useCallback(() => {
         const shellHeight = document.documentElement.clientHeight - (SHELL_PADDING * 2);
+        const shellWidth = document.documentElement.clientWidth - (SHELL_PADDING * 2);
 
         setRecoverStyle({
             cover: {
@@ -26,6 +27,9 @@ const Productions = (): JSX.Element => {
             },
             coverContent: {
                 height: shellHeight + 50
+            },
+            detailsContainer: {
+                width: shellWidth / 2
             }
         })
     }, [recover]);
@@ -116,7 +120,56 @@ const Productions = (): JSX.Element => {
                         )}
                         style={recoverStyle.coverContent}
                     >
-                        
+                        <div
+                            className="productions_recover__productions-details___container"
+                            style={recoverStyle.detailsContainer}
+                        >
+                            <img
+                                src="./icon/cancel.svg"
+                                className="productions_recover__productions-details___container____close"
+                                alt="menu"
+                                onClick={handleSelectItem}
+                            />
+                            <div className="productions-details">
+                                <div className="productions-details_meta">
+                                    <div className="productions-details_meta__infos">
+                                        <span className="productions-details_meta__infos___title">
+                                            Infinite Square
+                                        </span>
+                                        <span className="productions-details_meta__infos___date">
+                                            Septembre 2018 - En cours
+                                        </span>
+                                    </div>
+                                    <img src="/img/infinite-square_texte.png" alt="productions_logo" />
+                                </div>
+                                <div className="productions-details_section productions-details_section__presentation">
+                                    <span className="productions-details_section__title">
+                                        PRESENTATION
+                                    </span>
+                                    <p className="productions-details_section__content">
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                                    </p>
+                                </div>
+                                <div className="productions-details_section">
+                                    <span className="productions-details_section__title">
+                                        MISSIONS
+                                    </span>
+                                    <p className="productions-details_section__content">
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                                    </p>
+                                </div>
+                                <div className="productions-details_section">
+                                    <span className="productions-details_section__title">
+                                        COMPETENCES
+                                    </span>
+                                    <div className="productions-details_section__skills-list skills-list">
+                                        <div className="skills-list_item">Webpack</div>
+                                        <div className="skills-list_item">React</div>
+                                        <div className="skills-list_item">Html</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             )}
