fd8556228b94a1c8406acf57621cfda602921dcc
JGrenet <jeremy.grenet@epitech.eu>
2020-10-24 19:21:28 +0200

    feat(contact): start of contact and textfield input

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -1,9 +1,30 @@
 import React from "react";
+import TextField from "../textfield/TextField";
 
 const Contact = (): JSX.Element => {
     return (
         <div className="contact tab-content">
+            <div className="contact_title">
+                <h2>Contact</h2>
+            </div>
+            <div className="contact_content">
+                <div className="contact_content__container content">
+                    <span className="content_title">
+                        Formulaire de contact
+                    </span>
 
+                    <form action="#" className="content_form">
+                        <TextField
+                            placeholder="Prenom"
+                        />
+                    </form>
+                </div>
+                <div className="contact_content__container content">
+                    <div className="content_title">
+                        Vous pouvez me contacter également via
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/textfield/TextField.tsx
+++ src/components/textfield/TextField.tsx
@@ -1,9 +1,47 @@
-import React from "react";
+import React, { useCallback, useRef, useState } from "react";
 
-const TextField = (): JSX.Element => {
+interface TextFieldProps {
+    placeholder?: string;
+}
+
+const TextField = ({
+    placeholder
+}: TextFieldProps): JSX.Element => {
+    const [value, setValue] = useState("");
+    const inputContainerRef = useRef<HTMLDivElement>(null);
+
+    const handleOnChange = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
+        setValue(event.target.value);
+    }, [setValue])
+
+    const handleOnBlur = useCallback((event: React.FocusEvent<HTMLInputElement>) => {
+        if (!inputContainerRef.current) {
+            return;
+        }
+
+        if (!value) {
+            inputContainerRef.current.classList.remove("focused")
+        }
+    }, [value, inputContainerRef])
+
+    const handleOnFocus = useCallback((event: React.FocusEvent<HTMLInputElement>) => {
+        if (!inputContainerRef.current) {
+            return;
+        }
+        inputContainerRef.current.classList.add("focused")
+    }, [inputContainerRef])
+
     return (
-        <div className="contact tab-content">
-
+        <div className="textfield" ref={inputContainerRef}>
+            <input
+                className="textfield_input"
+                type="text"
+                placeholder={placeholder}
+                onChange={handleOnChange}
+                value={value}
+                onBlur={handleOnBlur}
+                onFocus={handleOnFocus}
+            />
         </div>
     )
 }
