74903049128a821b863bc00cd786bafc282f2003
JGrenet <jeremy.grenet@epitech.eu>
2020-11-27 20:26:39 +0100

    fix(global): minor fixes

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,26 +1,26 @@
 module.exports = {
   "extends": "@snowpack/app-scripts-react",
   "plugins": [
-    // ["@snowpack/plugin-run-script", {"cmd": "sass src:src --no-source-map", "watch": "$1 --watch"}],
-    ["@snowpack/plugin-webpack", {
-      extendConfig: (config) => {
-        // console.log(config.module.rules[1]);
+    ["@snowpack/plugin-run-script", {"cmd": "sass src:src --no-source-map", "watch": "$1 --watch"}],
+    // ["@snowpack/plugin-webpack", {
+    //   extendConfig: (config) => {
+    //     // console.log(config.module.rules[1]);
 
-        config.module.rules[1].use.push({loader: "postcss-loader", options: {
-          postcssOptions: {
-            plugins: [
-              [
-                "autoprefixer",
-                {
-                  // Options
-                },
-              ],
-            ],
-          },
-        }});
-        return config;
-      },
-    }]
+    //     config.module.rules[1].use.push({loader: "postcss-loader", options: {
+    //       postcssOptions: {
+    //         plugins: [
+    //           [
+    //             "autoprefixer",
+    //             {
+    //               // Options
+    //             },
+    //           ],
+    //         ],
+    //       },
+    //     }});
+    //     return config;
+    //   },
+    // }]
   ],
   "buildOptions": {
     "sourceMaps": true
